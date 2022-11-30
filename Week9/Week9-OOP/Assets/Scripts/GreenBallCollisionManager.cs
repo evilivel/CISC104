5659c45233382f1771b556c69bd5d3bea2d0bc12
evilivel <evilivel15@gmail.com>
2022-11-30 02:40:53 -0500

    added week 9 hw
    green ball get bigger
    red ball makes noise

---
+++ Week9/Week9-OOP/Assets/Scripts/GreenBallCollisionManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public class GreenBallCollisionManager : BallCollisionManager
+{
+    public override void CollideWithBall(GameObject OtherBall)
+    {
+        Debug.Log("Green Ball Collision Manager Function");
+
+        gameObject.transform.localScale += new Vector3(0.06f, 0.06f, 0.06f);
+    }
+}
+
---
+++ Week9/Week9-OOP/Assets/Scripts/RedBallCollisionManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class RedBallCollisionManager : BallCollisionManager
+{
+    public AudioSource bonk;
+
+    public override void CollideWithBall(GameObject OtherBall)
+    {
+        Debug.Log("Red Ball Collision Manager Function");
+
+        bonk.Play ();
+    }
+}
+
